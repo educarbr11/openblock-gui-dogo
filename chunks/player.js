6da8b042055103d1fce47e39b41428ae91b692d3
educarbr11 <educarbr11@users.noreply.github.com>
2026-08-03 13:16:20 +0000

    deploy: abacb0c465268d821b313558696d27acbf53f1b8

--- chunks/player.js
+++ chunks/player.js
@@ -1,7 +1,7 @@
 var GUI =
 (window["webpackJsonpGUI"] = window["webpackJsonpGUI"] || []).push([[9],{
 
-/***/ 2062:
+/***/ 2063:
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
@@ -95,7 +95,7 @@ react_dom__WEBPACK_IMPORTED_MODULE_3___default.a.render( /*#__PURE__*/react__WEB
 
 /***/ }),
 
-/***/ 2063:
+/***/ 2064:
 /***/ (function(module, exports, __webpack_require__) {
 
 exports = module.exports = __webpack_require__(22)(false);
@@ -118,7 +118,7 @@ exports.locals = {
 /***/ (function(module, exports, __webpack_require__) {
 
 
-var content = __webpack_require__(2063);
+var content = __webpack_require__(2064);
 
 if(typeof content === 'string') content = [[module.i, content, '']];
 
@@ -140,5 +140,5 @@ if(false) {}
 
 /***/ })
 
-},[[2062,0]]]);
+},[[2063,0]]]);
 //# sourceMappingURL=player.js.map
